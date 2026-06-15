a84756b98c17bdde505430b8a95509a784d62610
HossamSwilam <hossamswilam307@gmail.com>
2026-06-15 12:33:08 +0300

    Update gitignore and remove test_main

--- tests/test_main.cpp
+++
@@ -1,9 +0,0 @@
-#include <gtest/gtest.h>
-
-int main(int argc, char **argv) {
-    // 1. تهيئة مكتبة GoogleTest وقراءة أي شهادات أو آرغومنتس من الـ Command Line
-    ::testing::InitGoogleTest(&argc, argv);
-    
-    // 2. تشغيل كل الـ TESTs المكتوبة في كل الملفات المرتبطة (Linked) مع الملف ده
-    return RUN_ALL_TESTS();
-}
